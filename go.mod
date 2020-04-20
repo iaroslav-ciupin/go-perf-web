1bb24697be7723cd0a7641eb33a87f30b1fff3a7
Iaroslav Ciupin <iaroslav.ciupin@ellation.com>
2020-04-20 20:10:31 +0300

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/iaroslav-ciupin/go-perf-web
+
+go 1.14
+
+require github.com/gorilla/mux v1.7.4
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func compute(n int64) float64 {
+	arr := make([]float64, n)
+	for i := range arr {
+		arr[i] = rand.Float64()
+	}
+	sum := 0.0
+	sinSum := 0.0
+	for _, a := range arr {
+		sum += a
+		sinSum += math.Sin(2.0 * math.Pi * a)
+	}
+	//fmt.Println("Sum is", sum)
+	//fmt.Println("Sin sum is", sinSum)
+	return sum / sinSum
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	n := mux.Vars(r)["n"]
+	fmt.Println("received work", n)
+	num, err := strconv.Atoi(n)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	result := compute(int64(num))
+	w.Write([]byte(fmt.Sprintf("%f", result)))
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("Listening on :8081...")
+	router := mux.NewRouter()
+	router.
+		Methods(http.MethodGet).
+		Path("/work/{n}").
+		HandlerFunc(handler)
+	if err := http.ListenAndServe("localhost:8081", router); err != nil {
+		panic(err)
+	}
+}
